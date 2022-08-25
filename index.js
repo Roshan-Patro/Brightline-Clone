4e3ace04d5c9703e1ccb226ad933d439e33b5787
Dhunu <angelsaikia333@gmail.com>
2022-08-25 13:34:42 +0530

    trying something

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-import {navbar} from './modules/navbar.js';
+import { navbar } from "./modules/navbar.js";
 
-let navbar_container = document.querySelector('nav');
+let navbar_container = document.querySelector("nav");
 navbar_container.innerHTML = navbar();
 
 // navbar_container.innerHTML = navbarMediaQurrMedium();
 
+// Using window.scrollY to bring the variation in the navbar's background
+let nav = document.querySelector("nav");
 
- // Using window.scrollY to bring the variation in the navbar's background
- let nav = document.querySelector('nav');
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
 
- window.addEventListener('scroll', () => {
-     console.log(window.scrollY)
+    if (window.scrollY >= 100) {
+        nav.classList.add("active_nav");
+    } else {
+        nav.classList.remove("active_nav");
+    }
+});
 
-     if (window.scrollY >= 100) {
-         nav.classList.add('active_nav');
-     } else {
-         nav.classList.remove('active_nav');
-     }
- })
+// Using setInterval() to append different texts at the header portion
+let span = document.getElementById("span");
 
- // Using setInterval() to append different texts at the header portion
- let span = document.getElementById("span");
+let spanArr = [
+    "you",
+    "families",
+    "caregivers",
+    "therapists",
+    "coaches",
+    "providers",
+    "depression",
+    "employees",
+    "anxiety",
+];
 
- let spanArr = ['you', 'families', 'caregivers', 'therapists', 'coaches','providers', 'depression', 'employees', 'anxiety'];
-
- let count = 0;
- setInterval(function(){
-     span.innerText = spanArr[count];
-     count++;
-     if(count>(spanArr.length-1))
-     {
-         count = 0;
-     }
- },1000);
+let count = 0;
+setInterval(function () {
+    span.innerText = spanArr[count];
+    count++;
+    if (count > spanArr.length - 1) {
+        count = 0;
+    }
+}, 1000);
