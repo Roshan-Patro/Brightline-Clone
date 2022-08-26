b41008a67b9fc570ba7929b7a2886c339ba3cd9a
Shwetanshu89 <shekharahwet45@gmail.com>
2022-08-27 01:31:03 +0530

    added login.html

---
+++ scripts/login.js
@@ -0,0 +1,32 @@
+let form = document.querySelector("form");
+let signup_page=document.querySelector("#signup_page")
+let signupls=JSON.parse(localStorage.getItem("signup"))||[]
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    if(signupls.length==0){
+        alert("No user till now");
+        return;
+    }
+    let obj={
+        email:form.email.value,
+        password:form.password.value,
+        
+    }
+    let flag=false
+    signupls.forEach(function(ele){
+        if(ele.email==obj.email && ele.password==obj.password )
+        {
+            flag=true;
+            localStorage.setItem("login",JSON.stringify(ele));
+            alert("successfully logged in!")
+            window.location.href=`index.html`
+        }
+    })
+    if(flag===false){
+        alert("OOPS! User Does not Exist")
+    } 
+})
+signup_page.addEventListener("click",function(){
+    window.location.href=`signup.html`
+    
+})
---
+++ scripts/signup.js
@@ -0,0 +1,22 @@
+let form=document.querySelector("form")
+    
+    let signupls=JSON.parse(localStorage.getItem("signup"))||[]
+    // let signup_dtails=[]
+    form.addEventListener("submit",function(e){
+        e.preventDefault();
+        let obj={
+            email:form.email.value,
+            number:form.number.value,
+            password:form.password.value,
+        }
+        if(obj.email ==""|| obj.password==""||obj.number==""){
+           
+         alert("Field Empty")
+         
+         return;
+        } else{
+            signupls.push(obj)
+            localStorage.setItem("signup",JSON.stringify(signupls));
+            alert("Successfully Signed in!")
+        }
+    })
