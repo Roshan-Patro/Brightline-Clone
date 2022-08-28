70e00a1686da60866ffe7ffb8eafd2312ded07b0
jkumari25 <jagriti.sj15@gmail.com>
2022-08-28 14:07:26 +0530

    added resources file

---
+++ scripts/resources_page.js
@@ -0,0 +1,31 @@
+import { navbar } from "../modules/navbar.js";
+
+let navbar_container = document.querySelector("nav");
+navbar_container.innerHTML = navbar();
+
+// navbar_container.innerHTML = navbarMediaQurrMedium();
+
+// Using window.scrollY to bring the variation in the navbar's background
+let nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+
+    if (window.scrollY >= 100) {
+        nav.classList.add("active_nav");
+    } else {
+        nav.classList.remove("active_nav");
+    }
+});
+
+// code for redirecting to blog.html
+
+let elements = document.querySelectorAll("#big-container>div");
+
+// adding the event listener by looping
+
+elements.forEach(function(ele) {
+    ele.addEventListener('click', function() {
+        location.href ="blog.html";
+    });
+})
