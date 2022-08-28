35617b222e9c356dc6a91ee42d0881957cdfde3f
Dhunu <angelsaikia333@gmail.com>
2022-08-28 16:44:52 +0530

    1

--- scripts/theBrightlineExperience.js
+++ scripts/theBrightlineExperience.js
@@ -1,19 +1,21 @@
-import { navbar } from "../modules/navbar.js";
 
-let navbar_container = document.querySelector("nav");
-navbar_container.innerHTML = navbar();
-
-// navbar_container.innerHTML = navbarMediaQurrMedium();
-
-// Using window.scrollY to bring the variation in the navbar's background
-let nav = document.querySelector("nav");
-
-window.addEventListener("scroll", () => {
-    console.log(window.scrollY);
-
-    if (window.scrollY >= 100) {
-        nav.classList.add("active_nav");
-    } else {
-        nav.classList.remove("active_nav");
+document.querySelector("get_started").addEventListener("click",function(){
+    let logInData = JSON.parse(localStorage.getItem("login"));
+    if(logInData!==null)
+    {
+        location.href = "https://www.hellobrightline.com/employers";
     }
-});
+    else{
+        alert("Kindly please, Log in first!");
+    }
+})
+function clickBtn(){
+    let logInData = JSON.parse(localStorage.getItem("login"));
+    if(logInData!==null)
+    {
+        location.href = "https://www.hellobrightline.com/employers";
+    }
+    else{
+        alert("Kindly please, Log in first!");
+    }
+}
